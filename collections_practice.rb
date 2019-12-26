5cbc1eb7ad8389823f3a36dcb0e2caa47a21a63a
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-26 08:59:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+array=[1,3,7,5,2]
+words=["hello","hints","typing","i"]
 
+def sort_array_asc(array)
+array.sort
+end
+
+def sort_array_desc(array)
+  array.sort_by {|n| -n}
+end
+
+def sort_array_char_count(words)
+  words.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[1],array[2]=array[2],array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(names)
+names.each do |w|
+    w[2]="$"
+  end
+end
+
+
+  
